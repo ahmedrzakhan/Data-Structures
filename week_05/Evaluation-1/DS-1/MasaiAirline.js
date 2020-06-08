b14ad1099b727a30de92ebb3f066d32d904cf2d1
Ahmed Raza Khan <khanahmed925@gmail.com>
2020-06-08 23:06:01 +0530

    Masai Airlines

---
+++ week_05/Evaluation-1/DS-1/MasaiAirline.js
@@ -0,0 +1,25 @@
+function processData(input) {
+    //Enter your code here
+    
+    input = input.split("\n");
+    
+    var numberOfPeople = Number(input[0]);
+    // console.log("numberOfPeople", numberOfPeople);
+    
+    var weightOfCheckInBags = input[1].split(' ').map(Number);
+    // console.log("weightOfCheckInBags", weightOfCheckInBags);
+    
+    var weightOfHandBags = input[2].split(' ').map(Number);
+    // console.log("weightOfHandBags", weightOfHandBags);
+    
+    for ( var i = 0; i < numberOfPeople; i++ ) {
+        
+        if ( weightOfCheckInBags[i] > 15 || weightOfHandBags[i] > 7 ) {
+            console.log("Stop")
+        } else {
+            console.log("Boarding")
+            
+        }
+    }
+    
+} 
