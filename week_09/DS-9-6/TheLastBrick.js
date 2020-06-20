0f14a76abab0a178b4bfc7b67ceea321fbad89be
Ahmed Raza Khan <khanahmed925@gmail.com>
2020-06-20 11:33:05 +0530

    The Last Brick

---
+++ week_09/DS-9-6/TheLastBrick.js
@@ -0,0 +1,69 @@
+// Patlu and Motu works in a building construction, they have to put some number
+// of bricks N from one place to another, and started doing their work.
+
+// They want to find who will put the last brick.
+
+// They to follow a simple rule: - In the i'th round, Patlu puts i bricks whereas Motu
+// puts ix2 bricks.
+
+// There are only N bricks, you need to help find the challenge result to find who put
+// the last brick.
+
+// Input Format
+
+// First line contains an integer N.
+
+// Constraints
+
+// 1 ≤ N ≤ 10000
+
+// Output Format
+
+// Output "Patlu" (without the quotes) if Patlu puts the last bricks ,"Motu"(without
+//     the quotes) otherwise.
+
+// Sample Input 0
+
+// 13
+// Sample Output 0
+
+// Motu
+// Explanation 0
+
+// 13 bricks are there :
+
+// Patlu Motu
+
+// 1 2
+
+// 2 4
+
+// 3 1 ( Only 1 remains)
+
+// Hence, Motu puts the last one.
+function processData(input) {
+  //Enter your code here
+
+  var bricks = Number(input);
+  // console.log(bricks, typeof bricks);
+
+  var person = "Patlu";
+
+  for (var i = 1; i <= bricks; i++) {
+    bricks = bricks - i;
+
+    if (bricks <= 0) {
+      person = "Patlu";
+      break;
+    }
+
+    bricks = bricks - 2 * i;
+
+    if (bricks <= 0) {
+      person = "Motu";
+      break;
+    }
+  }
+
+  console.log(person);
+}
