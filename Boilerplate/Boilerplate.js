fb01cd6221df992d0b3e26fe1cba2e29e5d05711
Ahmed Raza Khan <khanahmed925@gmail.com>
2020-07-30 09:42:23 +0530

    JS Boilerplate

---
+++ Boilerplate/Boilerplate.js
@@ -0,0 +1,14 @@
+function runProgram(input){
+    // Write code here
+  }
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+      read += input;
+  });
+  process.stdin.on("end", function () {
+      read = read.replace(/\n$/,"")
+      read = read.replace(/\n$/,"")
+     runProgram(read);
+  });
