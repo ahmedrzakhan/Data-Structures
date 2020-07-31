91ba9c9e42a2ac097ee4bb31f6d55227215e50f9
Ahmed Raza Khan <khanahmed925@gmail.com>
2020-07-31 11:39:54 +0530

    Boilerplate updated

--- Boilerplate/Boilerplate.js
+++ Boilerplate/Boilerplate.js
@@ -1,14 +1,35 @@
-function runProgram(input){
-    // Write code here
-  }
-  process.stdin.resume();
-  process.stdin.setEncoding("ascii");
-  let read = "";
-  process.stdin.on("data", function (input) {
-      read += input;
-  });
-  process.stdin.on("end", function () {
-      read = read.replace(/\n$/,"")
-      read = read.replace(/\n$/,"")
-     runProgram(read);
-  });
+function runProgram(input) {
+  // Write code here
+  console.log(input * input);
+}
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+let read = "";
+process.stdin.on("data", function (input) {
+  read += input;
+});
+process.stdin.on("end", function () {
+  read = read.replace(/\n$/, "");
+  read = read.replace(/\n$/, "");
+  runProgram(read);
+});
+process.on("SIGINT", function () {
+  read = read.replace(/\n$/, "");
+  runProgram(read);
+  process.exit(0);
+});
+
+// function runProgram(input) {
+//   // Write code here
+// }
+// process.stdin.resume();
+// process.stdin.setEncoding("ascii");
+// let read = "";
+// process.stdin.on("data", function (input) {
+//   read += input;
+// });
+// process.stdin.on("end", function () {
+//   read = read.replace(/\n$/, "");
+//   read = read.replace(/\n$/, "");
+//   runProgram(read);
+// });
