1bb22457a35806aea439c6d0a2d0cf4a16813849
Ahmed Raza Khan <khanahmed925@gmail.com>
2020-08-19 17:42:07 +0530

    Question Added

--- week_17/Evaluation/DS-1/BeyondFactorial.js
+++ week_17/Evaluation/DS-1/BeyondFactorial.js
@@ -1,3 +1,40 @@
+
+// Beyond Factorial!
+// Description
+
+// Given an integer N , you have to find the natural logarithmic value of given 
+// integer's factorial i.e., ln(n!).
+
+// You have to print the result up to 4 digits after the decimal point.
+
+// Note: You are not allowed to use log built-in function to calculate directly ln(n!)
+//  but you can use it anywhere else to know the logarithmic value of a particular integer.
+//   Also, you can't use any direct formula or perform the task iteratively - Try achieving 
+//   the output recursively
+
+
+// Input
+// Input format
+
+// Single line consists an integer N
+
+// Constraints
+
+// 1 <= N <= 50
+
+// Output
+// Print the result in a single line.
+
+// Sample Input 1 
+
+// 3
+// Sample Output 1
+
+// 1.7918`
+// Hint
+
+// ln(x ∙ y) = ln(x) + ln(y)
+
 function runProgram(input) {
 
     let n = Number(input);
