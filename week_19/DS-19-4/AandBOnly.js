6dd291703ae64201a99b7fa6ea34f679aa45033f
Ahmed Raza Khan <khanahmed925@gmail.com>
2020-08-28 00:06:46 +0530

    question and optimized appraoch added

--- week_19/DS-19-4/AandBOnly.js
+++ week_19/DS-19-4/AandBOnly.js
@@ -1,3 +1,41 @@
+// a and b only
+// Description
+
+// You are given a string S that contains 3 characters: 'a', 'b', and '?'. Your
+// task is to convert it to a string which contains only 2 characters: 'a', 'b'.
+//  The converted string must follow following conditions:
+
+// 1. It should not contains two letters 'a' in a row.
+
+// 2. if there are many strings possible, the answer should be one which is the first
+// in alphabetical order
+
+// (Read sample test case for better understanding)
+
+// Input
+// Input Format
+
+// The first line contains non-empty string s consisting of 'a', 'b' and '?' characters.
+
+// Constraints
+
+// Length of s is at most 50.
+
+// Output
+// Output Format
+
+// Output the first in alphabetical order word
+
+// Sample Input 1
+
+// ?ba??b
+// Sample Output 1
+
+// ababab
+// Hint
+
+// Sample 1 Explanation
+
 function runProgram(input) {
   let arr = input.trim().split("");
 
@@ -26,6 +64,26 @@ function runProgram(input) {
   }
 
   console.log(arr.join(''));
+}
+
+runProgram("?ba??b");
+
+// Optimized Approach
+
+function runProgram(input) {
+  let arr = input.trim().split("");
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "?") {
+      if (arr[i + 1] === "a" || arr[i - 1] === "a") {
+        arr[i] = "b";
+      } else {
+        arr[i] = "a";
+      }
+    }
+  }
+
+  console.log(arr.join(""));
 }
 
 runProgram("?ba??b");
--- week_19/DS-19-4/MaximumSum.js
+++ week_19/DS-19-4/MaximumSum.js
@@ -1,18 +1,50 @@
+// Maximum Sum & Permutation
+// Description
+
+// Given an array of n elements. You can arrange the elements whichever way you
+//  want to. All you need to do is to find that permutation of integers of the 
+//  array in which the sum of index multiplied by arr[index] is maximum.
+
+// Basically maximum possible value of:
+
+// Σ arr[i] * i
+
+// Input
+// Input Format:
+
+// First-line contains n
+
+// Second-line contains n space-separated integers
+
+// Constraints:
+
+// n <= 100000
+
+// Output
+// Output the maximum sum for the given expression
+
+// Sample Input 1 
+
+// 4
+// 2 5 1 6
+// Sample Output 1
+// 30
+
 function runProgram(input) {
+
   input = input.split("\n");
 
   let n = Number(input[0]);
 
-  let arr = input[1].trim()
+  let sum = input[1]
+    .trim()
     .split(" ")
     .map(Number)
-    .sort((a, b) => a - b);
-
-//   console.log(n, arr);
-
-  let sum = arr
+    .sort((a, b) => a - b)
     .map((ele, index) => ele * index)
     .reduce((acc, ele) => acc + ele, 0);
+
+  //   console.log(n, arr);
 
   console.log(sum);
 }
