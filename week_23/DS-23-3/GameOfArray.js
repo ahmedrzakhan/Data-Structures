301bfd9e2d41a046550e2ca40472411514b56389
Ahmed Raza Khan <khanahmed925@gmail.com>
2020-09-23 14:30:52 +0530

    rotate using slice

--- week_23/DS-23-3/GameOfArray.js
+++ week_23/DS-23-3/GameOfArray.js
@@ -1,8 +1,8 @@
 // Game of array!
 // Description
 
-// Pavan is very fond of numbers.He made an array of numbers of length N, and he 
-// wants to know the pattern after rotating it to the left by k times. He got confused 
+// Pavan is very fond of numbers.He made an array of numbers of length N, and he
+// wants to know the pattern after rotating it to the left by k times. He got confused
 // in the middle since the array is very big can you help him to find the rotated array?
 
 // Input
@@ -15,7 +15,7 @@
 // Output
 // Print output array after k rotations.
 
-// Sample Input 1 
+// Sample Input 1
 
 // 6 4
 // 1 2 5 4 0 6
@@ -38,6 +38,28 @@ function runProgram(input) {
   }
 
   console.log(arr.join(" "));
+}
+
+// Using Slice
+
+function runProgram(input) {
+  input = input.trim().split("\n");
+
+  let [n, k] = input[0].trim().split(" ").map(Number);
+  // console.log("n, k", n, k);
+
+  let arr = input[1].trim().split(" ").map(Number);
+  // console.log("arr", arr);
+
+  k = k % n;
+
+  let firstSlice = arr.slice(k, n);
+  // console.log(firstSlice);
+
+  let secondSlice = arr.slice(0, k);
+  // console.log(secondSlice);
+
+  console.log(firstSlice.concat(secondSlice).join(" "));
 }
 
 runProgram("6 4\n1 2 5 4 0 6");
